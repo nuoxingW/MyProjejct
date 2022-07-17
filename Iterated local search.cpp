075f124817e32764b97716d66e8bf2c4d42a5929
nuoxingW <109012596+nuoxingW@users.noreply.github.com>
2022-07-17 14:06:16 +0800

    Update Iterated local search.cpp

--- Iterated local search.cpp
+++ Iterated local search.cpp
@@ -15,62 +15,55 @@ using namespace std;
 #define POT_SIZE 52 //散列点数量
 
 
-//散列点坐标
 typedef struct candidate
 {
     int x;
     int y;
 }pot, POTS;
 
-//优化值
+
 int** Delta;
 
-//解决方案
+
 typedef struct Solution
 {
-    int permutation[POT_SIZE]; //散列点排列
-    int cost;                        //该排列对应的总路线长度
+    int permutation[POT_SIZE]; 
+    int cost;                     
 }SOLUTION;
-// 计算邻域操作优化值 
+ 
 int calc_delta(int i, int k, int* tmp, POTS* pots);
 
-//计算两个散列点间距离
 int distance_2pot(pot c1, pot c2);
 
-//根据产生的散列点序列，计算旅游总距离
+
 int cost_total(int* pots_permutation, POTS* pots);
 
-//获取随机散列点排列, 用于产生初始解
 void random_permutation(int* pots_permutation);
 
-//颠倒数组中下标begin到end的元素位置, 用于two_opt邻域动作
 void swap_element(int* p, int begin, int end);
 
-//邻域动作 反转index_i <-> index_j 间的元素
+
 void two_opt_swap(int* pots_permutation, int* new_pots_permutation, int index_i, int index_j);
 
-//本地局部搜索，边界条件 max_no_improve
+
 void local_search(SOLUTION& best, POTS* pots, int max_no_improve);
 
-//判断接受准则
 bool AcceptanceCriterion(int* pots_permutation, int* old_pots_permutation, POTS* p_pots);
 
-//将散列点序列分成4块，然后按块重新打乱顺序。
-//用于扰动函数
+
+
 void double_bridge_move(int* pots_permutation, int* new_pots_permutation);
 
-//扰动
 void perturbation(POTS* pots, SOLUTION& best_solution, SOLUTION& current_solution);
 
-//迭代搜索
+
 void iterated_local_search(SOLUTION& best, POTS* pots, int max_iterations, int max_no_improve);
 
-// 更新Delta 
+
 void Update(int i, int k, int* tmp, POTS* pots);
 
-//散列点排列
 int permutation[POT_SIZE];
-//散列点坐标数组
+
 POTS pots[POT_SIZE];
 
 
@@ -90,7 +83,6 @@ int main()
     srand(1);
     int max_iterations = 600;
     int max_no_improve = 50;
-    //初始化指针数组 
     Delta = new int* [POT_SIZE];
     for (int i = 0; i < POT_SIZE; i++)
         Delta[i] = new int[POT_SIZE];
@@ -113,7 +105,7 @@ int main()
 
 
 
-//计算两个散列点间距离
+
 int distance_2pot(pot c1, pot c2)
 {
     int distance = 0;
@@ -122,15 +114,12 @@ int distance_2pot(pot c1, pot c2)
     return distance;
 }
 
-//根据产生的散列点序列，计算旅游总距离
-//所谓散列点序列，就是散列点先后访问的顺序，比如可以先访问ABC，也可以先访问BAC等等
-//访问顺序不同，那么总路线长度也是不同的
-//p_perm 散列点序列参数
+
 int cost_total(int* pots_permutation, POTS* pots)
 {
     int total_distance = 0;
     int c1, c2;
-    //逛一圈，看看最后的总距离是多少
+ 
     for (int i = 0; i < POT_SIZE; i++)
     {
         c1 = pots_permutation[i];
@@ -148,7 +137,7 @@ int cost_total(int* pots_permutation, POTS* pots)
     return total_distance;
 }
 
-//获取随机散列点排列
+
 void random_permutation(int* pots_permutation)
 {
     int i, r, temp;
@@ -171,7 +160,7 @@ void random_permutation(int* pots_permutation)
 
 
 
-//颠倒数组中下标begin到end的元素位置
+
 void swap_element(int* p, int begin, int end)
 {
     int temp;
@@ -186,7 +175,6 @@ void swap_element(int* p, int begin, int end)
 }
 
 
-//邻域动作 反转index_i <-> index_j 间的元素
 void two_opt_swap(int* pots_permutation, int* new_pots_permutation, int index_i, int index_j)
 {
     for (int i = 0; i < POT_SIZE; i++)
@@ -201,17 +189,7 @@ void two_opt_swap(int* pots_permutation, int* new_pots_permutation, int index_i,
 
 int calc_delta(int i, int k, int* tmp, POTS* pots) {
     int delta = 0;
-    /*
-                以下计算说明：
-                对于每个方案，翻转以后没必要再次重新计算总距离
-                只需要在翻转的头尾做个小小处理
 
-                比如：
-                有散列点序列   1-2-3-4-5 总距离 = d12 + d23 + d34 + d45 + d51 = A
-                翻转后的序列 1-4-3-2-5 总距离 = d14 + d43 + d32 + d25 + d51 = B
-                由于 dij 与 dji是一样的，所以B也可以表示成 B = A - d12 - d45 + d14 + d25
-                下面的优化就是基于这种原理
-    */
     if (i == 0)
     {
         if (k == POT_SIZE - 1)
@@ -252,13 +230,7 @@ int calc_delta(int i, int k, int* tmp, POTS* pots) {
 }
 
 
-/*
-    去重处理，对于Delta数组来说，对于散列点序列1-2-3-4-5-6-7-8-9-10，如果对3-5应用了邻域操作2-opt ， 事实上对于
-    7-10之间的翻转是不需要重复计算的。 所以用Delta提前预处理一下。
 
-    当然由于这里的计算本身是O（1） 的，事实上并没有带来时间复杂度的减少（更新操作反而增加了复杂度）
-    如果delta计算 是O（n）的，这种去重操作效果是明显的。
-*/
 
 void Update(int i, int k, int* tmp, POTS* pots) {
     if (i && k != POT_SIZE - 1) {
@@ -275,7 +247,7 @@ void Update(int i, int k, int* tmp, POTS* pots) {
                 Delta[j][l] = calc_delta(j, l, tmp, pots);
             }
         }
-    }// 如果不是边界，更新(i-1, k + 1)之间的 
+    }
     else {
         for (i = 0; i < POT_SIZE - 1; i++)
         {
@@ -284,13 +256,11 @@ void Update(int i, int k, int* tmp, POTS* pots) {
                 Delta[i][k] = calc_delta(i, k, tmp, pots);
             }
         }
-    }// 边界要特殊更新 
+    } 
 
 }
 
-//本地局部搜索，边界条件 max_no_improve
-//best_solution最优解
-//current_solution当前解
+
 void local_search(SOLUTION& best_solution, POTS* pots, int max_no_improve)
 {
     int count = 0;
@@ -312,12 +282,12 @@ void local_search(SOLUTION& best_solution, POTS* pots, int max_no_improve)
 
     do
     {
-        //枚举排列
+    
         for (i = 0; i < POT_SIZE - 1; i++)
         {
             for (k = i + 1; k < POT_SIZE; k++)
             {
-                //邻域动作
+          
                 two_opt_swap(best_solution.permutation, current_solution->permutation, i, k);
                 now_cost = inital_cost + Delta[i][k];
                 current_solution->cost = now_cost;
@@ -340,7 +310,7 @@ void local_search(SOLUTION& best_solution, POTS* pots, int max_no_improve)
 
     } while (count <= max_no_improve);
 }
-//判断接受准则
+
 bool AcceptanceCriterion(int* pots_permutation, int* old_pots_permutation, POTS* p_pots)
 {
     int acceptance = 500; //接受条件,与当前最解相差不超过acceptance
@@ -355,8 +325,6 @@ bool AcceptanceCriterion(int* pots_permutation, int* old_pots_permutation, POTS*
     return false;
 }
 
-//将散列点序列分成4块，然后按块重新打乱顺序。
-//用于扰动函数
 void double_bridge_move(int* pots_permutation, int* new_pots_permutation)
 {
     int temp_perm[POT_SIZE];
@@ -367,24 +335,22 @@ void double_bridge_move(int* pots_permutation, int* new_pots_permutation)
 
     int i;
     vector<int> v;
-    //第一块
+
     for (i = 0; i < pos1; i++)
     {
         v.push_back(pots_permutation[i]);
     }
 
-    //第二块
     for (i = pos3; i < POT_SIZE; i++)
     {
         v.push_back(pots_permutation[i]);
     }
-    //第三块
     for (i = pos2; i < pos3; i++)
     {
         v.push_back(pots_permutation[i]);
     }
 
-    //第四块
+
     for (i = pos1; i < pos2; i++)
     {
         v.push_back(pots_permutation[i]);
@@ -395,7 +361,7 @@ void double_bridge_move(int* pots_permutation, int* new_pots_permutation)
     {
         temp_perm[i] = v[i];
     }
-    //if accept判断是否接受当前解
+
     if (AcceptanceCriterion(pots_permutation, temp_perm, pots))
     {
         memcpy(new_pots_permutation, temp_perm, sizeof(temp_perm));//accept
@@ -404,21 +370,19 @@ void double_bridge_move(int* pots_permutation, int* new_pots_permutation)
 
 }
 
-//扰动
+
 void perturbation(POTS* pots, SOLUTION& best_solution, SOLUTION& current_solution)
 {
     double_bridge_move(best_solution.permutation, current_solution.permutation);
     current_solution.cost = cost_total(current_solution.permutation, pots);
 }
 
-//迭代搜索
-//max_iterations用于迭代搜索次数
-//max_no_improve用于局部搜索边界条件
+
 void iterated_local_search(SOLUTION& best_solution, POTS* pots, int max_iterations, int max_no_improve)
 {
     SOLUTION* current_solution = new SOLUTION;
 
-    //获得初始随机解
+  
     random_permutation(best_solution.permutation);
 
 
@@ -427,10 +391,10 @@ void iterated_local_search(SOLUTION& best_solution, POTS* pots, int max_iteratio
 
     for (int i = 0; i < max_iterations; i++)
     {
-        perturbation(pots, best_solution, *current_solution); //扰动+判断是否接受新解
-        local_search(*current_solution, pots, max_no_improve);//继续局部搜索
+        perturbation(pots, best_solution, *current_solution); 
+        local_search(*current_solution, pots, max_no_improve);
 
-        //找到更优解
+   
         if (current_solution->cost < best_solution.cost)
         {
             for (int j = 0; j < POT_SIZE; j++)
