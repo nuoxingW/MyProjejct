397baa129e559e21e2bc7a982d224ce5f5cd457a
nuoxingW <109012596+nuoxingW@users.noreply.github.com>
2022-08-03 00:28:57 +0800

    Add files via upload

--- Problems.java
+++ Problems.java
@@ -28,34 +28,61 @@ public class Problems {
 	double[][] dists;
 	int clusterPos[];
 	double[] prize;
- 
- 
- 
+  
+	FileReader data;
 
 	public void Problems(String fileName) throws FileNotFoundException,IOException {
-		Problems.fileName = fileName;
-		FileReader data;
+		Problems.fileName = fileName;	
 		data = new FileReader(fileName);
   		Scanner scan = new Scanner(data);
          readFileGTP(scan);
- 	} 
+         clusternum();
+  	} 
  
  
 	
  //读取文件
     private void readFileGTP(Scanner scan) {
-    	points = new ArrayList<>();
-    	nodeNum = (int) scan.nextDouble(); 
-    	System.out.println(nodeNum);
-    	ArrayList clusters = new ArrayList();
-    	System.out.println(nodeNum);
-     	pos = new double[nodeNum][2];
-    	for (int i = 0; i < nodeNum; i++) {
+    	 points = new ArrayList<>();
+         int count=0;
+         int s=0;
+         nodeNum=1600;
+       	 pos = new double[nodeNum][2];
+    	  while(scan.hasNext()){
+    		  count++;
+    		  if(count>15){
+    			  scan.nextDouble();
+    	     		pos[s][0] = scan.nextDouble();
+    	    		pos[s][1] = scan.nextDouble();
+    	    		System.out.print(pos[s][0]+"  ");
+    	    		System.out.println(pos[s][1]);
+    	    		s++;
+    	    		points.add(new Point(s, pos[s][0], pos[s][1]));
+    			    scan.nextLine();
+    			    System.out.println(s+"s的值");
+    			    if(s==317){
+    			    	System.out.println("跳出循环");
+    			    	break;
+    			    }
+    		  }else{		 
+    		     scan.nextDouble();
+    		  }
+    		  
+    	  }
+    	 
+    	// nodeNum=  scan.hasNext(); 
+    	 
+
+     	 //System.out.println(nodeNum);
+    
+
+/*    	for (int i = 1; i < nodeNum; i++) {
      		pos[i][0] = scan.nextDouble();
     		pos[i][1] = scan.nextDouble();
-    		System.out.println(pos[i][0]);
-    		points.add(new Point(i, pos[i][0], pos[i][1]));
-    	}
+    		System.out.println( pos[i][0]);
+    		System.out.println( pos[i][1]);
+    		
+    	}*/
     	calcuDistance();
 //    	dists = new double[nodeNum][nodeNum];
 //    	for (int i = 0; i < nodeNum; i++) {
@@ -66,21 +93,36 @@ public class Problems {
 //    			dists[j][i] = dists[i][j];
 //    		}
 //    	}
- 
+    //	scan.close();
+
+    }
+    
+    public void clusternum() throws FileNotFoundException{
+		FileReader data1 = new FileReader("C:\\Users\\Nuoxing.W\\Desktop\\36LIN318.GTP.txt");
+  		Scanner scan = new Scanner(data1);
+        ArrayList clusters = new ArrayList();
      	clusterNum = new int[nodeNum];
      	clusterPos = new int[nodeNum];
      	largestCluster = 0;
-     	double numOfCluster = scan.nextDouble();
-     	for (int i = 0; i < numOfCluster; i++) {
+        scan.skip("");
+        scan.skip("");
+      	int numOfCluster = scan.nextInt();
+      	System.out.println(numOfCluster+"===================");
+       	for (int i = 0; i < numOfCluster; i++) {
      		List<Integer> c = new ArrayList<>();
-     		int  m = (int) scan.nextDouble();
+     		int m =  scan.nextInt();
+     		scan.nextLine();
+     		System.out.println(m+"==========");
      		for (int j = 0; j < m; j++) {
-     			int node = (int) scan.nextDouble();
-     			System.out.println(clusters.size()+"clsters");
-     			System.out.println(nodeNum+"nodeNum");
-     			c.add(node - 1);
-     			clusterNum[node - 1] = clusters.size();
-     			clusterPos[node - 1] = c.size() - 1;
+     			scan.nextLine();
+     			int node =   scan.nextInt();
+/*     			System.out.println(node+"node");
+     			System.out.println(clusterNum.length+"length");
+     			System.out.println(clusters.size()+"nodeNum");*/
+     			System.out.println(node);
+     			c.add(node - 1);		                                              
+     				clusterNum[node - 1] = clusters.size();
+         			clusterPos[node - 1] = c.size() - 1;	    	
      		}
      		clusters.add(c);
      		if (c.size() > ((ArrayList) clusters.get(largestCluster)).size()) {
@@ -101,7 +143,6 @@ public class Problems {
      	a = scan.nextInt();
      	assert(a == -999);
     }
-    
   //to calculate the distance between cities
   	public void calcuDistance() {
   		dists = new double[nodeNum][nodeNum];
@@ -117,7 +158,7 @@ public class Problems {
   					if (fileName.toLowerCase().contains("att") && !Solution.withPrice) {
   						distance = Math.ceil(Math.sqrt(distance/10.0));
   					} else {
-  						distance = Math.sqrt(distance);
+  						distance = Math.sqrt(distance);	 
   					}
 					dists[i][j] = (int)(distance + 0.5);
   				}
