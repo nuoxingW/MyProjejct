2df81bad1c3b14eb31e40c4ad424c8960a80cf95
nuoxingW <109012596+nuoxingW@users.noreply.github.com>
2022-08-25 23:44:18 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -3,47 +3,43 @@ package Myproject;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReferenceArray;
  
 
 public class Solution implements  Comparable<Solution> {
 	public static boolean withPrice;
-	private static  int[] parent;//定义一个数组记录访问的边
-	private static  String[] vex ;//记录每次选择的边
+	//private static  int[] parent;//定义一个数组记录访问的边
+	//private static  String[] vex ;//记录每次选择的边
 	private int[] tops;   // 顶点集合
 	static Problems problem=new Problems();
     private static  double[][] matrix;    /**边集数组*/
   //  private List<Edge> edgeList = new ArrayList<Edge>();     //边集数组*
-
 	protected static String fileName="D:\\23GR229.GTP";
-	public Object Get_Vex() throws FileNotFoundException, IOException{
-		problem.read(fileName);
-		return problem.getPoints();
-	}
+  //边的结构体
+    private static class EData {
+        int start; // 边的起点
+        int end;   // 边的终点
+        double weight; // 边的权重
+ 
+        public EData(int tops, int tops2, double matrix) {
+            this.start = tops;
+            this.end = tops2;
+            this.weight = matrix;
+        }
+    };
+    
 	//矩阵获得某两点之间的距离
 	private static double Get_arc(int i,int j) throws FileNotFoundException, IOException{
-		problem.read(fileName);
  		return problem.getDists(i, j);
-	}
-	//一个簇所包含的节点号
-	public String Get_Cluster(int i) throws FileNotFoundException, IOException{
-		problem.read(fileName);
-	    String arr=Getarr(problem.getClusters(), i);
-		return arr;
-	}
-	//簇的个数
-	public  int  get_ClsuserSize() throws FileNotFoundException, IOException{
-		problem.read(fileName);
-		int  clusterLength=problem.getClustersLength();
-		return clusterLength;
-	}
-	//节点数
-	public   int getNodenum() throws FileNotFoundException, IOException{ 
-			problem.read(fileName);
-	        return problem.getNodeNum();
 	}
 	//某个簇集群包含的节点号
 	public  String Getarr(String arrString,int i) throws FileNotFoundException, IOException{ 
-		problem.read(fileName);
 	    String[]  arr=problem.getClusters().split("],");
 		   for(int j=0 ; j<arr.length;j++){
 				  arr[j]= arr[j].replace("[", "");
@@ -53,7 +49,6 @@ public class Solution implements  Comparable<Solution> {
 	}
 	// 节点号 所对应的X,Y坐标
 	public  String  getNodexy(int i) throws FileNotFoundException, IOException{
-		 problem.read(fileName);
 		 ArrayList<Point> array=new ArrayList<>();
 	     array=problem.getPoints();
 	     Point piont=array.get(i);
@@ -61,42 +56,12 @@ public class Solution implements  Comparable<Solution> {
 		return  piont.getX()+","+piont.getY();
 		
 	}
-	//每个节点所对应的prize
-	public  double  getPrize(int i) throws FileNotFoundException, IOException{
-		 problem.read(fileName);
-	     double prize=problem.getPrize(i);
-	     return prize;
-		
-	}
-	// prize[] 的长度
-	public  int  getPrizeLength() throws FileNotFoundException, IOException{
-		 problem.read(fileName);
-	     int prizeLength=problem.getPrizeLength();
-	     return prizeLength;
-		
-	}
-	public int getVex(int i) throws FileNotFoundException, IOException{
-		problem.read(fileName);
-	   return  problem.getVex(i);
-	}
-	public int distsLength() throws FileNotFoundException, IOException{
-		 problem.read(fileName);
-	     int distsLength=problem.getDistsLength();
-	     return distsLength;
-	}
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-           Solution s=new Solution();
-           s.createEdge(s.getNodenum());
-           s.kruskal();
-
-  }
 	@Override
 	public int  compareTo(Solution s) {
           return 1;
 	}
 	 // 初始化"顶点数"和"边数"
      private void createEdge(int index) throws FileNotFoundException, IOException {
-     	problem.read(fileName);
      	tops= new int[index]; 
         for(int i=0;i<index;i++){
              tops[i]= problem.getVex(i);  
@@ -108,20 +73,19 @@ public class Solution implements  Comparable<Solution> {
               }
          }
     }
-     
-     
-     public void kruskal() throws FileNotFoundException, IOException {
-         int index = 0;                   
-         int[] tends = new int[getNodenum()*getNodenum()];     // 用于保存"已有最小生成树"中每个顶点在该最小树中的终点。
-         EData[] rets = new EData[getNodenum()*getNodenum()];   
+     public void kruskal(int nodeNum) throws FileNotFoundException, IOException {
+         int index = 0;   
+       //  System.out.println(nodeNum+"!11111111111111");\
+        // System.out.println(nodeNum*node);
+         int[] tends = new int[52441];     // 用于保存"已有最小生成树"中每个顶点在该最小树中的终点。
+         EData[] rets = new EData[nodeNum*nodeNum];   
          EData[] edges;                      // 图对应的所有边
          // 获取"图中所有的边"
-         edges = getEdges();
+         edges = getEdges(nodeNum*nodeNum);
          // 将边按照"权"的大小进行排序(从小到大)
-         int num=getNodenum()*getNodenum();
-         System.out.println(edges[0].weight);
-         sortEdges(edges, num);
-         for (int i=0; i<100; i++) {
+          sortEdges(edges, nodeNum);
+         for (int i=0; i<nodeNum; i++) {
+        	 System.out.println(edges[i].start+","+edges[i].end);
              int p1 = getPosition(edges[i].start);      // 获取第i条边的"起点"的序号
              int p2 = getPosition(edges[i].end);        // 获取第i条边的"终点"的序号
   
@@ -141,13 +105,14 @@ public class Solution implements  Comparable<Solution> {
          for (int i = 0; i < index; i++){
              length += rets[i].weight;
          }
-         System.out.printf("Kruskal=%d: ", length);
+         System.out.print(length);
          for (int i = 0; i < index; i++){
-             System.out.printf("(%c,%c) ", rets[i].start, rets[i].end);
+             System.out.print(rets[i].start+","+rets[i].end);
+            
          }
          System.out.printf("\n"); 
      }
-     private int getPosition(int ch) {
+     private int getPosition(double ch) {
          for(int i=0; i<tops.length; i++){
              if(tops[i]==ch){
                  return i;
@@ -155,27 +120,15 @@ public class Solution implements  Comparable<Solution> {
          }
          return -1;
      }
-     //边的结构体
-     private static class EData {
-         int start; // 边的起点
-         int end;   // 边的终点
-         double weight; // 边的权重
-  
-         public EData(int tops, int tops2, double matrix) {
-             this.start = tops;
-             this.end = tops2;
-             this.weight = matrix;
-         }
-
- 
-     };
-     private EData[] getEdges() throws FileNotFoundException, IOException {
+     private EData[] getEdges(int nodeNum) throws FileNotFoundException, IOException {
          int index=0;
          EData[] edges;
-          edges = new EData[getNodenum()*getNodenum()];
+         int num=nodeNum*nodeNum;
+           edges = new EData[num];
          for (int i=0; i < tops.length; i++) {
              for (int j=i+1; j < tops.length; j++) {
                  if (matrix[i][j]!=Integer.MAX_VALUE) {
+                	 System.out.println(tops[i]+","+tops[j]);
                      edges[index++] = new EData(tops[i], tops[j], matrix[i][j]);
                   }
              }
@@ -184,18 +137,12 @@ public class Solution implements  Comparable<Solution> {
          return edges;
      }
      private void sortEdges(EData[] edges, int elen) {
-         for (int i=0; i<elen; i++) {
-             for (int j=i+1; j<elen; j++) {
-            	// System.out.println(i);
-            	 //System.out.println(edges[i].weight+"/"+edges[j].weight);
-                  if (edges[i].weight > edges[j].weight) {
-                     // 交换"边i"和"边j"
-                     EData tmp = edges[i];
-                     edges[i] = edges[j];
-                     edges[j] = tmp;
-                 }
-             }
-         }
+    	// List<Object> list=new ArrayList<>();
+    	     Arrays.sort(edges);
+    	  	 for(int i=0;i<elen;i++){
+        		 System.out.println(edges[i]);
+        	 }
+    	 
      }
      private int getEnd(int[] tends, int i) {
          while (tends[i] != 0){
@@ -203,6 +150,16 @@ public class Solution implements  Comparable<Solution> {
          }
          return i;
      }
-  
+     public static void main(String[] args) throws FileNotFoundException, IOException {
+            Solution s=new Solution();
+            problem.read(fileName);
+            int num=problem.getNodeNum();
+             System.out.println(num); 
+         
+          //  s.createEdge(problem.getNodeNum());
+            s.kruskal(num);
+        
+        
 
+   }
 }
